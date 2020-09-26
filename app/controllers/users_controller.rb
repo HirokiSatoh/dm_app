e40c9500fb22d35cf7d369bd3a889e447ea9e516
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-09-26 11:28:45 +0900

    users/controller修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user=User.find(prams[:id])
+    @user=User.find(params[:id])
     @currentUserEntry=Entry.where(user_id: current_user.id)
     @userEntry=Entry.where(user_id: @user.id)
     if @user.id==current_user.id
