3985f0dbf93235f8fa548acf63fb8a103dfc159a
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-09-26 11:27:13 +0900

    users/controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root to: "users#index"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, :only => [:index, :show]
-  root to: "home#top"
+
   resources :messages, :only => [:create]
   resources :rooms, :only => [:create, :show, :index]
 end
